48322cd2cd83608c1c44f361158498da8c649ecd
7cripter <47368552+7cripter@users.noreply.github.com>
2024-08-14 12:44:01 +0300

    ы

--- gasStation/gasStation.js
+++ gasStation/gasStation.js
@@ -70,4 +70,101 @@ export const gasStation = {
         de: 'Bitte schalten Sie den Motor vor dem Tanken aus',
         pl: 'Proszę wyłączyć silnik przed tankowaniem',
     },
+
+    fuelIsFull: {
+        ru: 'В этом Т/С уже максимум топлива',
+        ua: 'У цьому Т/С вже максимальна кількість палива',
+        en: "This vehicle's fuel tank is already full",
+        de: 'Dieser Fahrzeugtank ist bereits voll',
+        pl: 'W tym pojeździe zbiornik paliwa jest już pełny',
+        ge: 'ამ მანქანის საწვავის ავზი უკვე სავსეა',
+    },
+
+    noItemForRefuel: {
+        ru: 'У вас нет подходящего предмета для заправки этого Т/С',
+        ua: 'У вас немає відповідного предмета для заправки цього Т/С',
+        en: 'You do not have a suitable item to refuel this vehicle',
+        de: 'Sie haben keinen geeigneten Gegenstand zum Betanken dieses Fahrzeugs',
+        pl: 'Nie masz odpowiedniego przedmiotu do tankowania tego pojazdu',
+        ge: 'თქვენ არ გაქვთ შესაბამისი ნივთი ამ მანქანის საწვავით შევსებისთვის',
+    },
+
+    vehicleRefueledByItem: {
+        ru: 'Вы заправили Т/С на {{value}} л.',
+        ua: 'Ви заправили Т/С на {{value}} л.',
+        en: 'You refueled the vehicle by {{value}} liters.',
+        de: 'Sie haben das Fahrzeug um {{value}} Liter betankt.',
+        pl: 'Zatankowałeś pojazd o {{value}} litrów.',
+        ge: 'თქვენ მანქანა {{value}} ლიტრით შეავსეთ.',
+    },
+    
+    vehicleRechargedByItem: {
+        ru: 'Вы зарядили Т/С на {{value}} kWh.',
+        ua: 'Ви зарядили Т/С на {{value}} kWh.',
+        en: 'You recharged the vehicle by {{value}} kWh.',
+        de: 'Sie haben das Fahrzeug um {{value}} kWh aufgeladen.',
+        pl: 'Naładowałeś pojazd o {{value}} kWh.',
+        ge: 'თქვენ მანქანა {{value}} kWh-ით დამუხტეთ.',
+    },
+
+    engineTypeError: {
+        ru: 'Тип двигателя этой машины не предназначен для заправку этим видом топлива',
+        ua: 'Тип двигуна цієї машини не призначений для заправки цим видом палива',
+        en: 'The engine type of this car is not designed for fueling with this type of fuel',
+        de: 'Der Motortyp dieses Autos ist nicht für die Betankung mit dieser Kraftstoffart ausgelegt',
+        pl: 'Typ silnika tego samochodu nie jest przeznaczony do tankowania tego rodzaju paliwa',
+        ge: 'ამ მანქანის ძრავის ტიპი არ არის განკუთვნილი ამ ტიპის საწვავისათვის',
+    },
+
+    refueled: {
+        ru: 'Заправка завершена, спасибо за что выбрали нашу заправочную станцию, приезжайте ещё!',
+        ua: 'Заправка завершена, дякуємо що обрали нашу заправну станцію, приїжджайте ще!',
+        en: 'Refueling is complete, thank you for choosing our gas station, come again!',
+        de: 'Das Tanken ist abgeschlossen, danke, dass Sie unsere Tankstelle gewählt haben, kommen Sie wieder!',
+        pl: 'Tankowanie zakończone, dziękujemy za wybór naszej stacji benzynowej, zapraszamy ponownie!',
+        ge: 'ავზის შევსება დასრულდა, გმადლობთ ჩვენი ბენზინგასამართი სადგურის არჩევისთვის, კვლავ მობრძანდით!',
+    },
+    recharged: {
+        ru: 'Зарядка завершена, спасибо за что выбрали нашу заправочную станцию, приезжайте ещё!',
+        ua: 'Зарядка завершена, дякуємо що обрали нашу заправну станцію, приїжджайте ще!',
+        en: 'Recharging is complete, thank you for choosing our gas station, come again!',
+        de: 'Das Aufladen ist abgeschlossen, danke, dass Sie unsere Tankstelle gewählt haben, kommen Sie wieder!',
+        pl: 'Ładowanie zakończone, dziękujemy za wybór naszej stacji benzynowej, zapraszamy ponownie!',
+        ge: 'დამუხტვა დასრულდა, გმადლობთ ჩვენი ბენზინგასამართი სადგურის არჩევისთვის, კვლავ მობრძანდით!',
+    },
+
+    refueling: {
+        ru: 'Заправка...',
+        ua: 'Заправка...',
+        en: 'Refueling...',
+        de: 'Betanken...',
+        pl: 'Tankowanie...',
+        ge: 'გავსება...',
+    },
+
+    recharging: {
+        ru: 'Зарядка...',
+        ua: 'Зарядка...',
+        en: 'Recharging...',
+        de: 'Aufladen...',
+        pl: 'Ładowanie...',
+        ge: 'დამუხტვა...',
+    },
+
+    refuelingStarted: {
+        ru: 'Заправка началась, пожалуйста дождитесь полной заправки транспортного средства!',
+        ua: 'Заправка розпочалася, будь ласка, дочекайтеся повної заправки транспортного засобу!',
+        en: 'Refueling started, please wait for the vehicle to be fully refueled!',
+        de: 'Das Tanken hat begonnen, bitte warten Sie, bis das Fahrzeug vollständig betankt ist!',
+        pl: 'Tankowanie rozpoczęte, prosimy poczekać na pełne zatankowanie pojazdu!',
+        ge: 'ავზის შევსება დაიწყო, გთხოვთ, დაელოდოთ ტრანსპორტის სრული შევსებას!',
+    },
+    rechargingStarted: {
+        ru: 'Зарядка началась, пожалуйста дождитесь полной зарядки транспортного средства!',
+        ua: 'Зарядка розпочалася, будь ласка, дочекайтеся повної зарядки транспортного засобу!',
+        en: 'Recharging started, please wait for the vehicle to be fully charged!',
+        de: 'Das Aufladen hat begonnen, bitte warten Sie, bis das Fahrzeug vollständig aufgeladen ist!',
+        pl: 'Ładowanie rozpoczęte, prosimy poczekać na pełne naładowanie pojazdu!',
+        ge: 'დამუხტვა დაიწყო, გთხოვთ, დაელოდოთ ტრანსპორტის სრულ დამუხტვას!',
+    },
 };
