6e928ec4650fdfe0504d8e4b93f0511d3a629b76
str1x <str1x.official2077@gmail.com>
2024-10-03 02:08:21 +0300

    і

---
+++ gameEvents/hud.js
@@ -0,0 +1,32 @@
+export default {
+    raceHud: {
+        title: {
+            ru: 'Гонка',
+            ua: 'Гонка',
+            en: 'Race',
+            de: 'Rennen',
+            pl: 'Wyścig',
+        },
+        text: {
+            ru: 'Постарайтесь приехать первым',
+            ua: 'Спробуйте приїхати першим',
+            en: 'Try to finish first',
+            de: 'Versuchen Sie, als Erster anzukommen',
+            pl: 'Spróbuj przyjechać pierwszy',
+        },
+        yourCurrentPlace: {
+            ru: 'Ваше текущее положение в гонке',
+            ua: 'Ваше поточне положення в гонці',
+            en: 'Your current position in the race',
+            de: 'Ihre aktuelle Position im Rennen',
+            pl: 'Twoja obecna pozycja w wyścigu',
+        },
+        yourLap: {
+            ru: 'Текущий круг',
+            ua: 'Поточне коло',
+            en: 'Current lap',
+            de: 'Aktuelle Runde',
+            pl: 'Aktualne okrążenie',
+        },
+    },
+};
