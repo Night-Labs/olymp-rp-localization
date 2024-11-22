4f7506297ea671740ebbd726d26115224b98930f
str1x <str1x.official2077@gmail.com>
2024-11-22 16:32:24 +0200

    randomTruck?

---
+++ faction/randomTruck.js
@@ -0,0 +1,51 @@
+export default {
+    globalMessage: {
+        ru: '~r~Раздобыта информация о грузовике с материалами, он находится в красной зоне на карте. Найдите и угоните его и доставьте на базу, чтобы пополнить склад банды ценными ресурсами!',
+        ua: '~r~Отримано інформацію про вантажівку з матеріалами, вона знаходиться в червоній зоні на карті. Знайдіть і викрадіть її та доставте на базу, щоб поповнити склад банди цінними ресурсами!',
+        en: "~r~Information about a truck with materials has been obtained, it is located in the red zone on the map. Find and hijack it, then deliver it to the base to replenish your gang's warehouse with valuable resources!",
+        de: '~r~Es wurden Informationen über einen LKW mit Materialien erhalten, der sich in der roten Zone auf der Karte befindet. Finden und kapern Sie ihn und liefern Sie ihn zur Basis, um das Lager Ihrer Bande mit wertvollen Ressourcen aufzufüllen!',
+        pl: '~r~Zdobyto informacje o ciężarówce z materiałami, znajduje się ona w czerwonej strefie na mapie. Znajdź ją, przechwyć i dostarcz na bazę, aby uzupełnić magazyn gangu cennymi zasobami!',
+    },
+    truckFinded: {
+        ru: '~r~Кто то нашёл грузовик! Его локация транслируется в реальном времени на карте, вы можете перехватить этот грузовик!',
+        ua: '~r~Хтось знайшов вантажівку! Її локація транслюється в реальному часі на карті, ви можете перехопити цю вантажівку!',
+        en: '~r~Someone found the truck! Its location is being broadcast in real-time on the map, you can intercept this truck!',
+        de: '~r~Jemand hat den LKW gefunden! Sein Standort wird in Echtzeit auf der Karte übertragen, Sie können diesen LKW abfangen!',
+        pl: '~r~Ktoś znalazł ciężarówkę! Jej lokalizacja jest transmitowana na żywo na mapie, możesz ją przechwycić!',
+    },
+    signalLost: {
+        ru: '~r~Сигнал грузовика потерян! Как только восстановится увидишь на карте.',
+        ua: '~r~Сигнал вантажівки втрачено! Як тільки відновиться, ти побачиш її на карті.',
+        en: "~r~The truck's signal is lost! Once it's restored, you'll see it on the map.",
+        de: '~r~Das Signal des LKWs ist verloren! Sobald es wiederhergestellt ist, sehen Sie ihn auf der Karte.',
+        pl: '~r~Sygnał ciężarówki został utracony! Gdy tylko zostanie przywrócony, zobaczysz ją na mapie.',
+    },
+    missionCompleted: {
+        ru: 'Миссия выполнена! Поздравляем, ваша банда пополнилась ценными ресурсами!',
+        ua: 'Місія виконана! Вітаємо, ваша банда поповнилася цінними ресурсами!',
+        en: 'Mission accomplished! Congratulations, your gang has been replenished with valuable resources!',
+        de: 'Mission erfüllt! Glückwunsch, Ihre Bande wurde mit wertvollen Ressourcen aufgefüllt!',
+        pl: 'Misja zakończona! Gratulacje, twój gang został uzupełniony cennymi zasobami!',
+    },
+    eventEnd: {
+        ru: '~r~{{name}} доставил грузовик на базу своей банды.',
+        ua: '~r~{{name}} доставив вантажівку на базу своєї банди.',
+        en: "~r~{{name}} delivered the truck to their gang's base.",
+        de: '~r~{{name}} hat den LKW zur Basis seiner Bande gebracht.',
+        pl: '~r~{{name}} dostarczył ciężarówkę na bazę swojego gangu.',
+    },
+    playerEnteredVehicle: {
+        ru: 'Отвезите машину на точку разгрузки вашей банды. Ваша локация транслируется всем игрокам!',
+        ua: 'Відвезіть машину на точку розвантаження вашої банди. Ваша локація транслюється всім гравцям!',
+        en: "Take the car to your gang's unloading point. Your location is broadcast to all players!",
+        de: 'Bringen Sie das Fahrzeug zum Entladepunkt Ihrer Bande. Ihr Standort wird allen Spielern angezeigt!',
+        pl: 'Zawieź samochód do punktu rozładunku swojej bandy. Twoja lokalizacja jest transmitowana do wszystkich graczy!',
+    },
+    truckDestroyed: {
+        ru: '~r~Грузовик был уничтожен! Вы потеряли потенциальные ресурсы...',
+        ua: '~r~Вантажівку було знищено! Ви втратили потенційні ресурси...',
+        en: '~r~The truck was destroyed! You lost potential resources...',
+        de: '~r~Der LKW wurde zerstört! Sie haben potenzielle Ressourcen verloren...',
+        pl: '~r~Ciężarówka została zniszczona! Straciłeś potencjalne zasoby...',
+    },
+};
