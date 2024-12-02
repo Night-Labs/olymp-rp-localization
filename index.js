fd104825df440fe631fca2519f53f327b8666368
debug <maxim.11@ukr.net>
2024-12-02 04:34:00 +0200

    ы

--- index.js
+++ index.js
@@ -57,6 +57,7 @@ import referral from './referral';
 import buyer from './buyer/buyer';
 import { gasStation } from './gasStation/gasStation';
 import cayoPerico from './cayoPerico';
+import spawnSelector from './spawnSelector';
 
 export default {
     account,
@@ -118,4 +119,5 @@ export default {
     buyer,
     gasStation,
     cayoPerico,
+    spawnSelector
 };
---
+++ spawnSelector/index.js
@@ -0,0 +1,119 @@
+export default {
+    mainHeader: {
+        ru: 'Выбор точки спавна',
+        ua: 'Вибір точки спавна',
+        en: 'Choosing a spawn point',
+        de: 'Auswahl eines Spawnpunkts',
+        pl: 'Wybór punktu odrodzenia',
+    },
+    lastPos: {
+        ru: 'Место выхода',
+        ua: 'Місце виходу',
+        en: 'Exit point',
+        de: 'Ausgangspunkt',
+        pl: 'Punkt wyjścia',
+    },
+    inActiveTextDefault: {
+        ru: 'В настоящее время это невозможно',
+        ua: 'На данний час це неможливо',
+        en: 'This is currently impossible',
+        de: 'Dies ist derzeit nicht möglich',
+        pl: 'To jest obecnie niemożliwe',
+    },
+    vehicleRent: {
+        header: {
+            ru: 'Аренда транспорта',
+            ua: 'Оренда транспорту',
+            en: 'Vehicle rental',
+            de: 'Vermietung von Transportmitteln',
+            pl: 'Wynajem pojazdów',
+        },
+    },
+
+    play: {
+        ru: 'Играть',
+        ua: 'Грати',
+        en: 'Play',
+        de: 'Spielen',
+        pl: 'Grać',
+    },
+
+    spawnPoint: {
+        header: {
+            ru: 'Точка спавна',
+            ua: 'Точка спавна',
+            en: 'Spawn Point',
+            de: 'Spawnpunkt',
+            pl: 'Punkt odrodzenia',
+        },
+    },
+
+    house: {
+        header: {
+            ru: 'Дом',
+            ua: 'Будинок',
+            en: 'House',
+            de: 'Haus',
+            pl: 'Dom',
+        },
+        inActiveText: {
+            ru: 'У вас нет дома',
+            ua: 'У вас немає дома',
+            en: "You don't have a home",
+            de: 'Du hast kein Zuhause',
+            pl: 'Nie masz domu',
+        },
+    },
+
+    apartment: {
+        header: {
+            ru: 'Квартира',
+            ua: 'Квартира',
+            en: 'Apartment',
+            de: 'Wohnung',
+            pl: 'Mieszkanie',
+        },
+
+        inActiveText: {
+            ru: 'У вас нет квартиры',
+            ua: 'У вас немає квартири',
+            en: "You don't have an apartment",
+            de: 'Du hast keine Wohnung',
+            pl: 'Nie masz mieszkania',
+        },
+    },
+
+    faction: {
+        header: {
+            ru: 'Организация',
+            ua: 'Організація',
+            en: 'Organization',
+            de: 'Organisation',
+            pl: 'Organizacja',
+        },
+        inActiveText: {
+            ru: 'Вы не находитесь в организации',
+            ua: 'Ви не перебуваєте у організаціі',
+            en: 'You are not in the organization',
+            de: 'Sie sind nicht in der Organisation',
+            pl: 'Nie należysz do organizacji',
+        },
+    },
+
+    mansion: {
+        header: {
+            ru: 'Особняк',
+            ua: 'Особняк',
+            en: 'Mansion',
+            de: 'Villa',
+            pl: 'Rezydencja',
+        },
+        inActiveText: {
+            ru: 'У вашей семье нет особняка или вы не состоите в семье',
+            ua: "У вашоі сім'ї не має особняка або ви не перебуваєте у сім'ї",
+            en: 'Your family does not have a mansion or you are not in a family',
+            de: 'Ihre Familie hat keine Villa oder Sie sind nicht in einer Familie',
+            pl: 'Twoja rodzina nie ma rezydencji lub nie należysz do rodziny',
+        },
+    },
+};
