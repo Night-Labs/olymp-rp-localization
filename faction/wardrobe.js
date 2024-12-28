b3213f80bfd50c9b71de6352ca560bdcd925451f
str1x <str1x.official2077@gmail.com>
2024-12-28 23:30:51 +0200

    раздевалка апдейт

--- faction/wardrobe.js
+++ faction/wardrobe.js
@@ -5,7 +5,7 @@ export default {
         en: 'Changing Room',
         de: 'Umkleideraum',
         pl: 'Przebieralnia',
-        zh:'服装',
+        zh: '服装',
     },
     select: {
         ru: 'Закончить',
@@ -13,7 +13,7 @@ export default {
         en: 'Finish',
         de: 'Beenden',
         pl: 'Zakończ',
-       zh:'完成',
+        zh: '完成',
     },
     changeClothes: {
         ru: 'Поменять наряд',
@@ -21,7 +21,7 @@ export default {
         en: 'Change Outfit',
         de: 'Outfit wechseln',
         pl: 'Zmień strój',
-        zh:'换衣服',
+        zh: '换衣服',
     },
     startJob: {
         ru: 'Начать смену',
@@ -29,7 +29,7 @@ export default {
         en: 'Start Shift',
         de: 'Schicht beginnen',
         pl: 'Rozpocznij zmianę',
-       zh:'开始改变',
+        zh: '开始改变',
     },
     stopJob: {
         ru: 'Закончить смену',
@@ -37,7 +37,7 @@ export default {
         en: 'End Shift',
         de: 'Schicht beenden',
         pl: 'Zakończ zmianę',
-        zh:'完成更改',
+        zh: '完成更改',
     },
     //
     startedJobMessage: {
@@ -46,7 +46,7 @@ export default {
         en: 'You have started your shift',
         de: 'Sie haben Ihre Schicht begonnen',
         pl: 'Rozpocząłeś zmianę',
-        zh:'你开始了改变',
+        zh: '你开始了改变',
     },
     stoppedJobMessage: {
         ru: 'Вы закончили смену',
@@ -54,7 +54,7 @@ export default {
         en: 'You have ended your shift',
         de: 'Sie haben Ihre Schicht beendet',
         pl: 'Zakończyłeś zmianę',
-        zh:'你已经完成了改变',
+        zh: '你已经完成了改变',
     },
 
     // jobClothes
@@ -64,6 +64,13 @@ export default {
         en: 'Work Clothes #{{index}}',
         de: 'Arbeitskleidung #{{index}}',
         pl: 'Ubranie robocze #{{index}}',
-        zh:'工作服#{index}}',
+        zh: '工作服#{index}}',
+    },
+    jobClothesNothing: {
+        ru: 'Ничего',
+        ua: 'Нічого',
+        en: 'Nothing',
+        de: 'Nichts',
+        pl: 'Nic',
     },
 };
