0428faf33981224b49d99f64f8f99f82d9ac3fe1
str1x <str1x.official2077@gmail.com>
2025-01-07 00:00:58 +0200

    frisk layout done

---
+++ faction/cef/frisk.js
@@ -0,0 +1,37 @@
+export default {
+    search: {
+        ru: 'Обыск',
+        ua: 'Обшук',
+        en: 'Search',
+        de: 'Durchsuchen',
+        pl: 'Przeszukanie',
+    },
+    all_suspicious_items: {
+        ru: 'Все подозрительные вещи:',
+        ua: 'Усі підозрілі речі:',
+        en: 'All suspicious items:',
+        de: 'Alle verdächtigen Gegenstände:',
+        pl: 'Wszystkie podejrzane przedmioty:',
+    },
+    documents: {
+        ru: 'Документы',
+        ua: 'Документи',
+        en: 'Documents',
+        de: 'Dokumente',
+        pl: 'Dokumenty',
+    },
+    confiscate_items: {
+        ru: 'Изъять вещи',
+        ua: 'Вилучити речі',
+        en: 'Confiscate items',
+        de: 'Gegenstände beschlagnahmen',
+        pl: 'Skonfiskuj przedmioty',
+    },
+    pcs: {
+        ru: 'шт.',
+        ua: 'шт.',
+        en: 'pcs',
+        de: 'Stk.',
+        pl: 'szt.',
+    },
+};
