86c360d0b18d450f9977c147edd0db97cb231373
debug <maxim.11@ukr.net>
2025-01-15 20:34:35 +0200

    s

--- donate/index.js
+++ donate/index.js
@@ -15,8 +15,7 @@ export default {
             ru: 'Промокод',
             de: 'Promocode',
             pl: 'Kod promocyjny',
-           zh:'促销码',
-            
+            zh: '促销码',
         },
         enterPromocode: {
             ua: 'Введіть промокод',
@@ -24,7 +23,7 @@ export default {
             ru: 'Введите промокод',
             de: 'Promocode eingeben',
             pl: 'Wprowadź kod promocyjny',
-           zh:'输入促销码',
+            zh: '输入促销码',
         },
         forExample: {
             ua: 'Наприклад #olymprp',
@@ -32,7 +31,7 @@ export default {
             ru: 'Например #olymprp',
             de: 'Zum Beispiel #olymprp',
             pl: 'Na przykład #olymprp',
-            zh:'例如#olymprp',
+            zh: '例如#olymprp',
         },
         activate: {
             ua: 'Активувати',
@@ -40,7 +39,7 @@ export default {
             ru: 'Активировать',
             de: 'Aktivieren',
             pl: 'Aktywuj',
-            zh:'激活',
+            zh: '激活',
         },
         close: {
             ua: 'Закрити',
@@ -48,7 +47,7 @@ export default {
             ru: 'Закрыть',
             de: 'Schließen',
             pl: 'Zamknij',
-            zh:'关闭',
+            zh: '关闭',
         },
 
         doesNotExist: {
@@ -57,7 +56,7 @@ export default {
             en: 'Promo code does not exist',
             de: 'Der Promo-Code existiert nicht',
             pl: 'Kod promocyjny nie istnieje',
-           zh:'促销代码不存在',
+            zh: '促销代码不存在',
         },
         expired: {
             ru: 'У этого промо-кода истёк срок действия',
@@ -65,7 +64,7 @@ export default {
             en: 'This promo code has expired',
             de: 'Dieser Promo-Code ist abgelaufen',
             pl: 'Ten kod promocyjny wygasł',
-           zh:'这次促销的截止日期已经过去了',
+            zh: '这次促销的截止日期已经过去了',
         },
         activated: {
             ru: 'Вы уже активировали этот промо-код',
@@ -73,7 +72,7 @@ export default {
             en: 'You have already activated this promo code',
             de: 'Sie haben diesen Promo-Code bereits aktiviert',
             pl: 'Już aktywowałeś ten kod promocyjny',
-            zh:'您已经激活了此代码',
+            zh: '您已经激活了此代码',
         },
         maxPlayersActivated: {
             ru: 'Этот промокод ввело максимальное количество игроков',
@@ -81,7 +80,7 @@ export default {
             en: 'This promo code has been used by the maximum number of players',
             de: 'Dieser Promo-Code wurde von der maximalen Anzahl von Spielern verwendet',
             pl: 'Ten kod promocyjny został wykorzystany przez maksymalną liczbę graczy',
-            zh:'此促销代码已被最大数量的玩家使用',
+            zh: '此促销代码已被最大数量的玩家使用',
         },
         successActivated: {
             ru: 'Вы активировали промокод {{promocodeName}} на {{donate}} донат-валюты',
@@ -89,7 +88,7 @@ export default {
             en: 'You have activated the promo code {{promocodeName}} for {{donate}} donation currency',
             de: 'Sie haben den Promo-Code {{promocodeName}} für {{donate}} Spendenwährung aktiviert',
             pl: 'Aktywowałeś kod promocyjny {{promocodeName}} na {{donate}} walutę darowizn',
-           zh:'您已激活用于{{捐赠}}捐赠的促销代码',
+            zh: '您已激活用于{{捐赠}}捐赠的促销代码',
         },
     },
 
@@ -100,7 +99,7 @@ export default {
             en: 'Buy ?',
             de: 'Kaufen ?',
             pl: 'Kup ?',
-            zh:'购买',
+            zh: '购买',
         },
 
         buyItemForDonate: {
@@ -109,7 +108,7 @@ export default {
             en: 'Buy {{name}} for {{donate}} Olymp-Coins',
             de: 'Kaufen {{name}} für {{donate}} Olymp-Coins',
             pl: 'Kup {{name}} za {{donate}} Olymp-Coins',
-           zh:'用捐赠的方式购买Olymp币',
+            zh: '用捐赠的方式购买Olymp币',
         },
 
         yes: {
@@ -118,7 +117,7 @@ export default {
             en: 'Yes!',
             de: 'Ja!',
             pl: 'Tak!',
-            zh:'是的',
+            zh: '是的',
         },
 
         no: {
@@ -127,7 +126,7 @@ export default {
             en: 'No',
             de: 'Nein',
             pl: 'Nie',
-           zh:'不',
+            zh: '不',
         },
     },
 };
