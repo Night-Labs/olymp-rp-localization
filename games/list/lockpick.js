ec4b5e640a5bcdbc3b0df4a4b54416a2bc39be4b
debug <maxim.11@ukr.net>
2025-03-20 01:25:00 +0200

    lockpick

---
+++ games/list/lockpick.js
@@ -0,0 +1,40 @@
+export default {
+    name: {
+        ru: 'ВЗЛОМАЙТЕ ЗАМОК',
+        ua: 'ЗЛАМАЙТЕ ЗАМОК',
+        en: 'PICK THE LOCK',
+        de: 'KNACK DAS SCHLOSS',
+        pl: 'OTWÓRZ ZAMEK',
+    },
+    desc: {
+        ru: 'Вам требуется взломать замок, для этого поочерёдно приподнимите пины замка, удерживая зажим, будьте внимательны, дёргающийся пин указывает на то что он пока не может быть приподнят.',
+        ua: 'Вам потрібно зламати замок, для цього по черзі підніміть піни замка, утримуючи затискач, будьте уважні, пін, що смикається, вказує на те, що його поки що не можна підняти.',
+        en: 'You need to pick the lock. To do this, lift the lock pins one by one while holding the tension wrench. Be careful, a twitching pin indicates that it cannot be lifted yet.',
+        de: 'Du musst das Schloss knacken. Hebe dazu nacheinander die Stifte des Schlosses an, während du den Spanner hältst. Sei vorsichtig, ein zuckender Stift zeigt an, dass er noch nicht angehoben werden kann.',
+        pl: 'Musisz otworzyć zamek. Aby to zrobić, podnoś kolejno kołki zamka, trzymając napinacz. Uważaj, drgający kołek wskazuje, że nie można go jeszcze podnieść.',
+    },
+
+    hook: {
+        ru: 'Перемещение отмычки',
+        ua: 'Переміщення відмички',
+        en: 'Moving the lock pick',
+        de: 'Bewegen des Dietrichs',
+        pl: 'Przesunięcie wytrycha',
+    },
+
+    wrench: {
+        ru: 'Зажим',
+        ua: 'Затискач',
+        en: 'Tension Wrench',
+        de: 'Spanner',
+        pl: 'Napinacz',
+    },
+
+    exit: {
+        ru: 'Выйти из игры',
+        ua: 'Вийти з гри',
+        en: 'Exit game',
+        de: 'Spiel verlassen',
+        pl: 'Wyjdź z gry',
+    },
+};
