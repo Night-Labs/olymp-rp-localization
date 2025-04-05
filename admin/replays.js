4de17be2f0820577ae5fd183a75a1f518a0cfab6
str1x <str1x.official2077@gmail.com>
2025-04-05 17:22:55 +0300

    s

--- admin/replays.js
+++ admin/replays.js
@@ -20,4 +20,11 @@ export default {
         de: 'Replay des Spielers ID: {{id}} Index: {{index}} wurde beendet. Wenn sich der Spieler verdächtig verhalten hat, ergreifen Sie Maßnahmen',
         pl: 'Powtórka Gracza ID: {{id}} Indeks: {{index}} została zakończona. Jeśli gracz zachowywał się podejrzanie, podejmij działania',
     },
+    replayRecordedMessageForAdmins: {
+        ru: '~r~[OLYMP ADMIN] Только что был записан реплей для игрока {{name}}, посмотреть его можно по команде {{command}}',
+        ua: '~r~[OLYMP ADMIN] Щойно було записано реплей для гравця {{name}}, переглянути його можна за командою {{command}}',
+        en: '~r~[OLYMP ADMIN] A replay has just been recorded for player {{name}}, you can view it with the command {{command}}',
+        de: '~r~[OLYMP ADMIN] Es wurde gerade ein Replay für Spieler {{name}} aufgezeichnet, du kannst es mit dem Befehl {{command}} ansehen',
+        pl: '~r~[OLYMP ADMIN] Właśnie został nagrany replay dla gracza {{name}}, możesz go obejrzeć za pomocą komendy {{command}}',
+    },
 };
