54ec1c27143ac1af261d3905b247864a075b39e9
debug <maxim.11@ukr.net>
2025-04-21 18:52:06 +0300

    s

--- phone/index.js
+++ phone/index.js
@@ -1,7 +1,24 @@
 import { cef } from './cef';
 import { server } from './server';
 
+// Pages
+import apps from './pages/apps';
+import vehicles from './pages/vehicles';
+import phone from './pages/phone';
+import messages from './pages/messages';
+import call from './pages/call';
+import gps from './pages/gps';
+import settings from './pages/settings';
+
 export default {
     ...cef,
     ...server,
+
+    apps,
+    vehicles,
+    phone,
+    messages,
+    call,
+    gps,
+    settings,
 };
---
+++ phone/pages/messages.js
@@ -0,0 +1,56 @@
+export default {
+    tabs: {
+        conversations: {
+            ru: 'Сообщения',
+            ua: 'Повідомлення',
+            en: 'Messages',
+            de: 'Nachrichten',
+            pl: 'Wiadomości',
+        },
+        new: {
+            ru: 'Контакты',
+            ua: 'Контакти',
+            en: 'Contacts',
+            de: 'Kontakte',
+            pl: 'Kontakty',
+        },
+    },
+
+    conversations: {
+        title: {
+            ru: 'Список сообщений',
+            ua: 'Список повідомлень',
+            en: 'Message List',
+            de: 'Nachrichtenliste',
+            pl: 'Lista wiadomości',
+        },
+    },
+
+    new: {
+        title: {
+            ru: 'Список контактов',
+            ua: 'Список контактів',
+            en: 'Contact List',
+            de: 'Kontaktliste',
+            pl: 'Lista kontaktów',
+        },
+    },
+
+    conversation: {
+        sayHello: {
+            ru: 'Здесь пока нет сообщений <br> Напишите первое сообщение чтобы начать диалог!',
+            ua: 'Тут поки немає повідомлень <br> Напишіть перше повідомлення, щоб почати діалог!',
+            en: 'There are no messages yet <br> Write the first message to start a dialogue!',
+            de: 'Hier sind noch keine Nachrichten <br> Schreibe die erste Nachricht, um einen Dialog zu starten!',
+            pl: 'Nie ma jeszcze wiadomości <br> Napisz pierwszą wiadomość, aby rozpocząć dialog!',
+        },
+
+        enterMessage: {
+            ru: 'Введите ваше сообщение',
+            ua: 'Введіть ваше повідомлення',
+            en: 'Enter your message',
+            de: 'Gib deine Nachricht ein',
+            pl: 'Wpisz swoją wiadomość',
+        },
+    },
+};
