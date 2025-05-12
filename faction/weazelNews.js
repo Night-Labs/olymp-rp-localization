7af36ff8f11e7b44417b2aa61a09b741756a7c56
str1x <str1x.official2077@gmail.com>
2025-05-13 00:37:05 +0300

    s

--- faction/weazelNews.js
+++ faction/weazelNews.js
@@ -1,10 +1,10 @@
 export default {
     adMessage: {
-        ru: '!{#DA8815}[РЕКЛАМА] {{message}} | {{publisherName}} Телефон: {{phoneNumber}}',
-        ua: '!{#DA8815}[РЕКЛАМА] {{message}} | {{publisherName}} Телефон: {{phoneNumber}}',
-        en: '!{#DA8815}[AD] {{message}} | {{publisherName}} Phone: {{phoneNumber}}',
-        de: '!{#DA8815}[WERBUNG] {{message}} | {{publisherName}} Telefon: {{phoneNumber}}',
-        pl: '!{#DA8815}[REKLAMA] {{message}} | {{publisherName}} Telefon: {{phoneNumber}}',
+        ru: '!{#DA8815}[РЕКЛАМА] {{message}} | {{fromName}} Телефон: {{phoneNumber}}',
+        ua: '!{#DA8815}[РЕКЛАМА] {{message}} | {{fromName}} Телефон: {{phoneNumber}}',
+        en: '!{#DA8815}[AD] {{message}} | {{fromName}} Phone: {{phoneNumber}}',
+        de: '!{#DA8815}[WERBUNG] {{message}} | {{fromName}} Telefon: {{phoneNumber}}',
+        pl: '!{#DA8815}[REKLAMA] {{message}} | {{fromName}} Telefon: {{phoneNumber}}',
     },
     adMessage2: {
         ru: '!{#A06A18}Отредактировал сотрудник Weazel News {{name}}.',
@@ -12,5 +12,82 @@ export default {
         en: '!{#A06A18}Edited by Weazel News staff member {{name}}.',
         de: '!{#A06A18}Bearbeitet von Weazel News Mitarbeiter {{name}}.',
         pl: '!{#A06A18}Edytowane przez pracownika Weazel News {{name}}.',
+    },
+
+    
+
+    cef: {
+        titleBase: {
+            ru: 'ОБЪЯВЛЕНИЯ',
+            ua: 'ОГОЛОШЕННЯ',
+            en: 'ANNOUNCEMENTS',
+            de: 'ANKÜNDIGUNGEN',
+            pl: 'OGŁOSZENIA',
+        },
+        titleEdit: {
+            ru: 'РЕДАКТИРОВАНИЕ',
+            ua: 'РЕДАГУВАННЯ',
+            en: 'EDITING',
+            de: 'BEARBEITUNG',
+            pl: 'EDYCJA',
+        },
+        editTextByRules: {
+            ru: 'Отредактируйте текст по всем правилам',
+            ua: 'Відредагуйте текст за всіма правилами',
+            en: 'Edit the text according to all the rules',
+            de: 'Bearbeiten Sie den Text nach allen Regeln',
+            pl: 'Edytuj tekst zgodnie ze wszystkimi zasadami',
+        },
+        acceptButton: {
+            ru: 'Отправить',
+            ua: 'Надіслати',
+            en: 'Submit',
+            de: 'Senden',
+            pl: 'Wyślij',
+        },
+        declineButton: {
+            ru: 'Отклонить',
+            ua: 'Відхилити',
+            en: 'Decline',
+            de: 'Ablehnen',
+            pl: 'Odrzuć',
+        },
+        startEdit: {
+            ru: 'Начать проверку',
+            ua: 'Почати перевірку',
+            en: 'Start review',
+            de: 'Überprüfung starten',
+            pl: 'Rozpocznij sprawdzanie',
+        },
+        from: {
+            ru: 'От',
+            ua: 'Від',
+            en: 'From',
+            de: 'Von',
+            pl: 'Od',
+        },
+        statuses: {
+            editing: {
+                ru: 'Редактируется {{name}}',
+                ua: 'Редагується {{name}}',
+                en: 'Editing {{name}}',
+                de: '{{name}} wird bearbeitet',
+                pl: 'Edytowanie {{name}}',
+            },
+            declined: {
+                ru: 'Отклонено {{name}}',
+                ua: 'Відхилено {{name}}',
+                en: 'Declined {{name}}',
+                de: 'Abgelehnt {{name}}',
+                pl: 'Odrzucono {{name}}',
+            },
+            published: {
+                ru: 'Опубликовано {{name}}',
+                ua: 'Опубліковано {{name}}',
+                en: 'Published {{name}}',
+                de: 'Veröffentlicht {{name}}',
+                pl: 'Opublikowano {{name}}',
+            },
+        },
     },
 };
