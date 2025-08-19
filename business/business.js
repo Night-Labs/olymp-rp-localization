3ea5bfc267cc99494c4a273a7782ec9bfe5bf4a1
debug <47368552+debugerovich@users.noreply.github.com>
2025-08-19 17:09:04 +0300

    Create business.js

---
+++ business/business.js
@@ -0,0 +1,179 @@
+export default {
+    tabs: {
+        info: {
+            name: {
+                ru: 'Информация',
+                ua: 'Інформація',
+                en: 'Information',
+                de: 'Informationen',
+                pl: 'Informacje',
+            },
+
+            balance: {
+                ru: 'Баланс',
+                ua: 'Баланс',
+                en: 'Balance',
+                de: 'Guthaben',
+                pl: 'Saldo',
+            },
+            price: {
+                ru: 'Цена',
+                ua: 'Ціна',
+                en: 'Price',
+                de: 'Preis',
+                pl: 'Cena',
+            },
+            owner: {
+                ru: 'Владелец',
+                ua: 'Власник',
+                en: 'Owner',
+                de: 'Eigentümer',
+                pl: 'Właściciel',
+            },
+            rentPrice: {
+                ru: 'Стоимость аренды в сутки',
+                ua: 'Вартість оренди за добу',
+                en: 'Daily rental price',
+                de: 'Tagesmietpreis',
+                pl: 'Cena wynajmu za dobę',
+            },
+            commercialBalance: {
+                ru: 'Ваш коммерческий баланс',
+                ua: 'Ваш комерційний баланс',
+                en: 'Your commercial balance',
+                de: 'Ihr Geschäftsguthaben',
+                pl: 'Twoje saldo komercyjne',
+            },
+            paidFor: {
+                ru: 'Оплачено до',
+                ua: 'Оплачено до',
+                en: 'Paid until',
+                de: 'Bezahlt bis',
+                pl: 'Opłacono do',
+            },
+            tax: {
+                ru: 'Налог',
+                ua: 'Податок',
+                en: 'Tax',
+                de: 'Steuer',
+                pl: 'Podatek',
+            },
+            todaysTakings: {
+                ru: 'Выручка за сегодня',
+                ua: 'Виручка за сьогодні',
+                en: "Today's takings",
+                de: 'Heutige Einnahmen',
+                pl: 'Dzisiejsze obroty',
+            },
+            averageBill: {
+                ru: 'Средний чек',
+                ua: 'Середній чек',
+                en: 'Average bill',
+                de: 'Durchschnittsrechnung',
+                pl: 'Średni rachunek',
+            },
+
+            buy: {
+                ru: 'Купить',
+                ua: 'Купити',
+                en: 'Buy',
+                de: 'Kaufen',
+                pl: 'Kup',
+            },
+            sell: {
+                ru: 'Продать',
+                ua: 'Продати',
+                en: 'Sell',
+                de: 'Verkaufen',
+                pl: 'Sprzedać',
+            },
+        },
+        statistics: {
+            name: {
+                ru: 'Статистика',
+                ua: 'Статистика',
+                en: 'Statistics',
+                de: 'Statistik',
+                pl: 'Statystyki',
+            },
+        },
+
+        logs: {
+            name: {
+                ru: 'Логи',
+                ua: 'Логи',
+                en: 'Logs',
+                de: 'Protokolle',
+                pl: 'Logi',
+            },
+            message: {
+                ru: 'Сообщение',
+                ua: 'Повідомлення',
+                en: 'Message',
+                de: 'Nachricht',
+                pl: 'Wiadomość',
+            },
+            type: {
+                ru: 'Тип',
+                ua: 'Тип',
+                en: 'Type',
+                de: 'Typ',
+                pl: 'Typ',
+            },
+            time: {
+                ru: 'Время',
+                ua: 'Час',
+                en: 'Time',
+                de: 'Zeit',
+                pl: 'Czas',
+            },
+        },
+    },
+
+    control: {
+        name: {
+            ru: 'Управление',
+            ua: 'Управління',
+            en: 'Control',
+            de: 'Steuerung',
+            pl: 'Sterowanie',
+        },
+        products: {
+            name: {
+                ru: 'Товары',
+                ua: 'Товари',
+                en: 'Products',
+                de: 'Produkte',
+                pl: 'Produkty',
+            },
+            inStorage: {
+                name: 'Сейчас в хранилище',
+                ua: 'Зараз у сховищі',
+                en: 'Currently in storage',
+                de: 'Derzeit im Lager',
+                pl: 'Obecnie w magazynie',
+            },
+            pricePerUnit: {
+                name: 'Цена за единицу',
+                ua: 'Ціна за одиницю',
+                en: 'Price per unit',
+                de: 'Preis pro Einheit',
+                pl: 'Cena za jednostkę',
+            },
+            pricePerDelivery: {
+                name: 'Цена за доставку',
+                ua: 'Ціна за доставку',
+                en: 'Price per delivery',
+                de: 'Preis pro Lieferung',
+                pl: 'Cena za dostawę',
+            },
+            desc: {
+                name: 'Для успешной работы бизнесс всегда должен иметь товары, не забывайте вовремя их пополнять иначе ваше предприятие просто не сможет работать!',
+                ua: 'Для успішної роботи бізнес завжди повинен мати товари, не забувайте вчасно їх поповнювати, інакше ваше підприємство просто не зможе працювати!',
+                en: "For successful operation, a business must always have goods. Don't forget to replenish them on time, otherwise your enterprise simply won't be able to function!",
+                de: 'Für einen erfolgreichen Betrieb muss ein Unternehmen immer Waren vorrätig haben. Vergessen Sie nicht, diese rechtzeitig aufzufüllen, sonst kann Ihr Unternehmen einfach nicht funktionieren!',
+                pl: 'Dla pomyślnego działania biznes zawsze musi mieć towary. Nie zapomnij uzupełniać ich na czas, w przeciwnym razie Twoje przedsiębiorstwo po prostu nie będzie mogło funkcjonować!',
+            },
+        },
+    },
+};
