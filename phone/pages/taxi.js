9f6715f2da0bca7f8427b163d179273fb29894fb
debug <47368552+debugerovich@users.noreply.github.com>
2026-01-07 15:11:16 +0200

    Update taxi.js

--- phone/pages/taxi.js
+++ phone/pages/taxi.js
@@ -174,12 +174,124 @@ export default {
         de: 'Bitte warten Sie, bis ein Fahrer Ihre Bestellung annimmt, verfügbare Fahrer: ',
         pl: 'Proszę czekać, aż kierowca przyjmie Twoje zamówienie, dostępnych kierowców: ',
     },
-    
+
     ordersList: {
         ru: 'Список заказов',
         ua: 'Список замовлень',
         en: 'Orders list',
         de: 'Bestellliste',
         pl: 'Lista zamówień',
+    },
+
+    startShiftTitle: {
+        ru: '<span style="color: var(--primary)">Downtown Cab Co.</span> всегда нуждается в умелых и ответственных водителях, знающих своё дело; если ты один из таких — присоединяйся к нам!',
+        ua: '<span style="color: var(--primary)">Downtown Cab Co.</span> завжди потребує вправних і відповідальних водіїв, які знають свою справу; якщо ти один із таких — приєднуйся до нас!',
+        en: '<span style="color: var(--primary)">Downtown Cab Co.</span> is always looking for skilled and responsible drivers who know their craft. If you\'re one of them, join us!',
+        de: '<span style="color: var(--primary)">Downtown Cab Co.</span> ist immer auf der Suche nach kompetenten und verantwortungsbewussten Fahrern, die ihr Handwerk verstehen. Wenn du einer von ihnen bist, schließ dich uns an!',
+        pl: '<span style="color: var(--primary)">Downtown Cab Co.</span> zawsze potrzebuje zręcznych i odpowiedzialnych kierowców, którzy znają się na swojej pracy — jeśli jesteś jednym z nich, dołącz do nas!',
+    },
+
+    desc: {
+        ru: 'Для работы вам необходим <span style="color: var(--primary)">{{level}} уровень</span> в штате, <span style="color: var(--primary)">действующие права категории B</span>, а также личная или арендованная <span style="color: var(--primary)">машина</span>, подходящая под требования работы. Вам будут доступны заказы как вашего класса, так и ниже. Также обращайте внимание на <span style="color: var(--primary)">теги</span> в информации о заказе, километраже и оплате. В случае если не будет заказов от игроков, вам будут доступны заказы от <span style="color: var(--primary)">NPC</span>. Класс вашего такси и, как следствие, оплата будут определены <span style="color: var(--primary)">автоматически</span> согласно цене вашего авто, а также вашему уровню таксиста.',
+        ua: 'Для роботи вам необхідний <span style="color: var(--primary)">{{level}} рівень</span> у штаті, <span style="color: var(--primary)">дійсні права категорії B</span>, а також власний або орендований <span style="color: var(--primary)">автомобіль</span>, що відповідає вимогам роботи. Вам будуть доступні замовлення як вашого класу, так і нижче. Також звертайте увагу на <span style="color: var(--primary)">теги</span> в інформації про замовлення, кілометраж і оплату. У разі відсутності замовлень від гравців вам будуть доступні замовлення від <span style="color: var(--primary)">NPC</span>. Клас вашого таксі та, відповідно, оплата будуть визначені <span style="color: var(--primary)">автоматично</span> згідно з ціною вашого авто та рівнем таксиста.',
+        en: 'To work, you need a <span style="color: var(--primary)">{{level}} level</span> in the state, <span style="color: var(--primary)">a valid Category B driver\'s license</span>, as well as a personal or rented <span style="color: var(--primary)">vehicle</span> that meets the job requirements. You will have access to orders of your class and lower. Also pay attention to the <span style="color: var(--primary)">tags</span> in the order details, distance, and payment. If there are no player orders available, you will receive orders from <span style="color: var(--primary)">NPCs</span>. The class of your taxi and, consequently, the payment will be determined <span style="color: var(--primary)">automatically</span> based on the price of your vehicle and your taxi driver level.',
+        de: 'Für die Arbeit benötigen Sie einen <span style="color: var(--primary)">{{level}}-Level</span> im Staat, einen <span style="color: var(--primary)">gültigen Führerschein der Klasse B</span> sowie ein eigenes oder gemietetes <span style="color: var(--primary)">Fahrzeug</span>, das den Arbeitsanforderungen entspricht. Ihnen stehen Aufträge Ihrer Klasse sowie niedrigerer Klassen zur Verfügung. Achten Sie außerdem auf die <span style="color: var(--primary)">Tags</span> in den Auftragsinformationen, der Kilometerzahl und der Bezahlung. Sollten keine Aufträge von Spielern verfügbar sein, erhalten Sie Aufträge von <span style="color: var(--primary)">NPCs</span>. Die Klasse Ihres Taxis und damit die Bezahlung werden <span style="color: var(--primary)">automatisch</span> anhand des Fahrzeugpreises und Ihres Taxifahrer-Levels festgelegt.',
+        pl: 'Do pracy wymagany jest <span style="color: var(--primary)">{{level}} poziom</span> w stanie, <span style="color: var(--primary)">ważne prawo jazdy kategorii B</span> oraz własny lub wynajęty <span style="color: var(--primary)">samochód</span> spełniający wymagania pracy. Będą dostępne zlecenia zarówno twojej klasy, jak i niższych. Zwracaj także uwagę na <span style="color: var(--primary)">tagi</span> w informacjach o zleceniu, kilometrażu i wynagrodzeniu. W przypadku braku zleceń od graczy dostępne będą zlecenia od <span style="color: var(--primary)">NPC</span>. Klasa twojej taksówki, a co za tym idzie wynagrodzenie, zostaną określone <span style="color: var(--primary)">automatycznie</span> na podstawie ceny pojazdu oraz twojego poziomu taksówkarza.',
+    },
+
+    carFrom: {
+        ru: 'Авто от',
+        ua: 'Авто від',
+        en: 'Car from',
+        de: 'Auto ab',
+        pl: 'Auto od',
+    },
+
+    levelFrom: {
+        ru: 'Уровень от',
+        ua: 'Рівень від',
+        en: 'Level from',
+        de: 'Level ab',
+        pl: 'Poziom od',
+    },
+
+    salary: {
+        ru: 'Оплата',
+        ua: 'Оплата',
+        en: 'Payment',
+        de: 'Bezahlung',
+        pl: 'Wynagrodzenie',
+    },
+
+    startShift: {
+        ru: 'Начать смену',
+        ua: 'Почати зміну',
+        en: 'Start shift',
+        de: 'Schicht beginnen',
+        pl: 'Rozpocząć zmianę',
+    },
+
+    endShift: {
+        ru: 'Закончить смену',
+        ua: 'Закінчити зміну',
+        en: 'End shift',
+        de: 'Schicht beenden',
+        pl: 'Zakończyć zmianę',
+    },
+
+    unavailable: {
+        ru: 'Недоступно',
+        ua: 'Недоступно',
+        en: 'Unavailable',
+        de: 'Nicht verfügbar',
+        pl: 'Niedostępne',
+    },
+
+    clientLocation: {
+        ru: 'Местоположение клиента',
+        ua: 'Місцезнаходження клієнта',
+        en: 'Client location',
+        de: 'Standort des Kunden',
+        pl: 'Lokalizacja klienta',
+    },
+
+    destinationPoint: {
+        ru: 'Точка назначения',
+        ua: 'Точка призначення',
+        en: 'Destination point',
+        de: 'Zielpunkt',
+        pl: 'Punkt docelowy',
+    },
+
+    acceptOffer: {
+        ru: 'Принять вызов',
+        ua: 'Прийняти виклик',
+        en: 'Accept call',
+        de: 'Anruf annehmen',
+        pl: 'Przyjąć wezwanie',
+    },
+
+    acceptedByOtherDriver: {
+        ru: 'Принят другим водителем',
+        ua: 'Прийнято іншим водієм',
+        en: 'Accepted by another driver',
+        de: 'Von einem anderen Fahrer angenommen',
+        pl: 'Przyjęte przez innego kierowcę',
+    },
+
+    completedOrders: {
+        ru: 'Выполнено заказов',
+        ua: 'Виконано замовлень',
+        en: 'Completed orders',
+        de: 'Abgeschlossene Aufträge',
+        pl: 'Zrealizowane zlecenia',
+    },
+    
+    earndedMoney: {
+        ru: 'Заработано денег',
+        ua: 'Зароблено грошей',
+        en: 'Money earned',
+        de: 'Verdientes Geld',
+        pl: 'Zarobione pieniądze',
     },
 };
