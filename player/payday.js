e1d3bba4e76a5a921f19208c0a3483e15e42b004
str1x <str1x.official2077@gmail.com>
2026-03-02 14:40:50 +0200

    Update payday.js

--- player/payday.js
+++ player/payday.js
@@ -1,11 +1,11 @@
 export default {
     messageSalary: {
-        ru: '~o~[PAYDAY] ~w~Вы получили зарплату в размере {{money}}',
-        ua: '~o~[PAYDAY] ~w~Ви отримали зарплату в розмірі {{money}}',
-        en: '~o~[PAYDAY] ~w~You received a salary of {{money}}',
-        de: '~o~[PAYDAY] ~w~Du hast ein Gehalt von {{money}} erhalten',
-        pl: '~o~[PAYDAY] ~w~Otrzymałeś wynagrodzenie w wysokości {{money}}',
-        zh:'~o~[PAYDAY]~w~你收到了{{money}}的薪水',
+        ru: '~o~[PAYDAY] ~w~Вы получили зарплату в размере {{money}} и {{exp}} XP',
+        ua: '~o~[PAYDAY] ~w~Ви отримали зарплату в розмірі {{money}} і {{exp}} XP',
+        en: '~o~[PAYDAY] ~w~You received a salary of {{money}} and {{exp}} XP',
+        de: '~o~[PAYDAY] ~w~Du hast ein Gehalt von {{money}} und {{exp}} XP erhalten',
+        pl: '~o~[PAYDAY] ~w~Otrzymałeś wynagrodzenie w wysokości {{money}} i {{exp}} XP',
+        zh:'~o~[PAYDAY]~w~你收到了{{money}}的薪水和{{exp}} XP',
     },
     messageNotTime: {
         ru: '~o~[PAYDAY] ~w~Для получения пейдея нужно отыграть минимум {{time}} минут',
@@ -16,11 +16,11 @@ export default {
        zh:'~o~[PAYDAY]~w~您需要至少玩{{time}}分钟才能获得发薪日',
     },
     messageBenefits: {
-        ru: '~o~[PAYDAY] ~w~Вы получили пособие по безработице: {{benefits}}',
-        ua: '~o~[PAYDAY] ~w~Ви отримали допомогу по безробіттю: {{benefits}}',
-        en: '~o~[PAYDAY] ~w~You received unemployment benefits: {{benefits}}',
-        de: '~o~[PAYDAY] ~w~Du hast Arbeitslosengeld erhalten: {{benefits}}',
-        pl: '~o~[PAYDAY] ~w~Otrzymałeś zasiłek dla bezrobotnych: {{benefits}}',
-        zh:'~o~[PAYDAY]~w~你领取失业救济金:{{benefits}}',
+        ru: '~o~[PAYDAY] ~w~Вы получили пособие по безработице: {{benefits}} и {{exp}} XP',
+        ua: '~o~[PAYDAY] ~w~Ви отримали допомогу по безробіттю: {{benefits}} і {{exp}} XP',
+        en: '~o~[PAYDAY] ~w~You received unemployment benefits: {{benefits}} and {{exp}} XP',
+        de: '~o~[PAYDAY] ~w~Du hast Arbeitslosengeld erhalten: {{benefits}} und {{exp}} XP',
+        pl: '~o~[PAYDAY] ~w~Otrzymałeś zasiłek dla bezrobotnych: {{benefits}} i {{exp}} XP',
+        zh:'~o~[PAYDAY]~w~你领取失业救济金:{{benefits}} 和 {{exp}} XP',
     },
 };
