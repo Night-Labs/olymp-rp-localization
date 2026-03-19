b8fc2b703f32c6d7a7a36de0d6d0de168d286157
str1x <str1x.official2077@gmail.com>
2026-03-19 19:37:39 +0200

    s

--- faction/server/npcs/index.js
+++ faction/server/npcs/index.js
@@ -1,9 +1,11 @@
 import lspd from './lspd';
+import sheriff from './sheriff';
 import ems from './ems';
 import gang from './gang';
 
 export default {
     lspd,
+    sheriff,
     gang,
     ems,
 };
